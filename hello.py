efe76901592b25be8a20903d019cf071b1ee0a53
theintthandarnaing <39290010+theintthandarnaing@users.noreply.github.com>
2018-06-10 22:46:33 -0700

    change commit

--- hello.py
+++ hello.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def theint():
     return "Hello World"
 
-@app.route("/tiide")
+@app.route("/theint")
 def tiide():
-    return "Welcome to TIIDE World"
+    return "Welcome to Theint"
